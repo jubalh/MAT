c1607cb3ad1bac02abd844516ce2dc052fd0cc12
jvoisin <pouicpouicpouic@gmail.com>
2011-08-16 18:15:57 +0200

    Add fogottent stuff

---
+++ mat/hachoir_editor/__init__.py
@@ -0,0 +1,8 @@
+from field import (
+    EditorError, FakeField)
+from typed_field import (
+    EditableField, EditableBits, EditableBytes,
+    EditableInteger, EditableString,
+    createEditableField)
+from fieldset import EditableFieldSet, NewFieldSet, createEditor
+
