abbf9308b0bf2f01ef716d1e357ec3c5e0939990
jvoisin <pouicpouicpouic@gmail.com>
2011-08-21 21:03:07 +0200

    Still improving images support

--- mat/images.py
+++ mat/images.py
@@ -8,6 +8,8 @@ import parser
 class JpegStripper(parser.GenericParser):
     '''
         represents a jpeg file
+        remaining :
+        http://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/CanonRaw.html
     '''
     def _should_remove(self, field):
         '''
@@ -16,7 +18,9 @@ class JpegStripper(parser.GenericParser):
         name = field.name
         if name.startswith('comment'):
             return True
-        elif name in ("photoshop", "exif", "adobe"):
+        elif name in ('photoshop', 'exif', 'adobe', 'app12'):
+            return True
+        elif name in ('icc'):  # should we remove the icc profile ?
             return True
         else:
             return False
@@ -25,15 +29,20 @@ class JpegStripper(parser.GenericParser):
 class PngStripper(parser.GenericParser):
     '''
         represents a png file
+        see : http://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/PNG.html
     '''
     def _should_remove(self, field):
         '''
             return True if the field is compromizing
         '''
         name = field.name
-        if name.startswith("text[") or name.startswith('utf8_text'):
+        if name.startswith('text['):  # textual meta
             return True
-        elif name == "time":
+        elif name.startswith('utf8_text['):  # uncompressed adobe crap
+            return True
+        elif name.startswith('compt_text['):  # compressed adobe crap
+            return True
+        elif name == "time":  # timestamp
             return True
         else:
             return False
