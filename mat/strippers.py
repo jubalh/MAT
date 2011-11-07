7e43701c21b04338ce3c44f5d1ea918fa4315047
jvoisin <julien.voisin@dustri.org>
2011-11-07 11:41:22 +0100

    Comments

--- mat/strippers.py
+++ mat/strippers.py
@@ -20,7 +20,7 @@ STRIPPERS = {
     'application/officeopenxml': office.OpenXmlStripper,
 }
 
-try:
+try:  # pdf support
     import poppler
     import cairo
     STRIPPERS['application/x-pdf'] = office.PdfStripper
@@ -29,20 +29,19 @@ except ImportError:
     print('Unable to import python-poppler and/or python-cairo: no pdf \
         support')
 
-try:
+    try:  # mutangen-python : audio format support
     import mutagen
     STRIPPERS['audio/x-flac'] = audio.FlacStripper
     STRIPPERS['audio/vorbis'] = audio.OggStripper
 except ImportError:
     print('Unable to import python-mutagen: limited audio format support')
 
-try:
-    # check if exiftool is installed on the system
+try:  # check if exiftool is installed on the system
     subprocess.Popen('exiftool', stdout=open('/dev/null'))
     import exiftool
     STRIPPERS['image/jpeg'] = exiftool.JpegStripper
     STRIPPERS['image/png'] = exiftool.PngStripper
-except:
+except:  # if exiftool is not installed, use hachoir
     print('Unable to find exiftool: limited images support')
     STRIPPERS['image/jpeg'] = images.JpegStripper
     STRIPPERS['image/png'] = images.PngStripper
