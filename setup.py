073ddcd0d824488fb9060d1f3dd237e2a75af360
jvoisin <julien.voisin@dustri.org>
2012-01-01 19:43:36 +0100

    Add more metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python
 
+import os
+
 from distutils.core import setup
 
-setup(name='MAT',
-      version='0.1',
-      description='Metadata Anonymisation Toolkit',
-      author='Julien (jvoisin) Voisin',
-      author_email='julien.voisin@dustri.org',
-      license='GPLv2',
-      url='https://mat.boum.org',
-      packages=['mat', 'mat.hachoir_editor', 'mat.bencode',
-          'mat.tarfile'],
-      scripts=['mat-cli', 'mat-gui'],
-      data_files=[
-          ( 'share/applications', [ 'mat.desktop' ] ),
-          ( 'share/mat', ['FORMATS'] ),
-          ( 'share/doc/mat', ['README', 'TODO'] ),
-          ]
-     )
+from mat import mat
+
+#Remove MANIFEST file, since distutils
+#doesn't properly update it when
+#the contents of directories changes.
+if os.path.exists('MANIFEST'):
+    os.remove('MANIFEST')
+
+setup(
+    name              = 'MAT',
+    version           = mat.__version__,
+    description       = 'Metadata Anonymisation Toolkit',
+    long_decription   = 'A Metadata Anonymisation Toolkit in Python, using python-hachoir',
+    author            = mat.__author__,
+    author_email      = 'julien.voisin@dustri.org',
+    plateforms        = 'linux'
+    license           = 'GPLv2',
+    url               = 'https://mat.boum.org',
+    packages          = ['mat', 'mat.hachoir_editor', 'mat.bencode', 'mat.tarfile'],
+    scripts           = ['mat-cli', 'mat-gui'],
+    data_files        = [
+        ( 'share/applications', [ 'mat.desktop' ] ),
+        ( 'share/mat', ['FORMATS'] ),
+        ( 'share/doc/mat', ['README', 'TODO'] ),
+    ],
+)
